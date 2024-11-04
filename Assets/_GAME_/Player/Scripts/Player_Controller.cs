19d052cdf4c041e912c0b0b688c27209462a734b
Levi-24 <leventemajor36@gmail.com>
2024-11-04 17:01:10 +0100

    rollback to last safe state cause everything got fucked

--- Assets/_GAME_/Player/Scripts/Player_Controller.cs
+++ Assets/_GAME_/Player/Scripts/Player_Controller.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 [SelectionBase]
 public class Player_Controller : MonoBehaviour
 {
-    [Header("Character Attributes")]
-    [SerializeField] public int maxHealth = 100;
-    private int currentHealth;
-    [SerializeField] private int level = 5;
-
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 200f;
     [SerializeField] private float dashSpeed = 1000f;
@@ -48,26 +43,5 @@ public class Player_Controller : MonoBehaviour
         isDashing = true;
         dashEndTime = Time.time + dashDuration;
         nextDashTime = Time.time + dashCooldown;
-    }
-
-    void Start()
-    {
-        currentHealth = maxHealth;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        Debug.Log("Player Health: " + currentHealth);
-
-        if (currentHealth <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-        gameObject.SetActive(false);
     }
 }
